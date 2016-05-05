cd72df01f9d274b88dd8e228e54b725359eeb2c9
sachin619 <echo.smb@gmail.com>
2016-05-05 17:24:04 +0530

    about us

--- wp-content/themes/canvas/page-about-us.php
+++ wp-content/themes/canvas/page-about-us.php
@@ -5,4 +5,89 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+get_header();
+?>
+<style type="text/css">
+	.feature-box h3{color: #000!important;}
+	.feature-box h3 span.subtitle{color: #000!important;}
+</style>
+		<!-- Page Title
+		============================================= -->
+		<section id="page-title">
 
+			<div class="container clearfix">
+				<h1>About Us</h1>				
+				<ol class="breadcrumb">
+					<li><a href="#">Home</a></li>
+					<li class="active">Contact</li>
+				</ol>
+			</div>
+
+		</section><!-- #page-title end -->
+
+		<!-- Content
+		============================================= -->
+		<section id="content">
+
+			<div class="content-wrap">
+
+				<div class="container clearfix">
+
+			
+
+					<div class="clear"></div>
+					<br><br>
+
+					<!-- Contact Info
+					============================================= -->
+					<div class="row clear-bottommargin">
+
+						<div class="col-md-3 col-sm-6 bottommargin clearfix">
+							<div class="feature-box fbox-center fbox-bg fbox-plain">
+								<div class="fbox-icon">
+									<a href="#"><i class="icon-map-marker2"></i></a>
+								</div>
+								<h3>Our Headquarters<span class="subtitle">Melbourne, Australia</span></h3>
+							</div>
+						</div>
+
+						<div class="col-md-3 col-sm-6 bottommargin clearfix">
+							<div class="feature-box fbox-center fbox-bg fbox-plain">
+								<div class="fbox-icon">
+									<a href="#"><i class="icon-phone3"></i></a>
+								</div>
+								<h3>Speak to Us<span class="subtitle">(123) 456 7890</span></h3>
+							</div>
+						</div>
+
+						<div class="col-md-3 col-sm-6 bottommargin clearfix">
+							<div class="feature-box fbox-center fbox-bg fbox-plain">
+								<div class="fbox-icon">
+									<a href="#"><i class="icon-skype2"></i></a>
+								</div>
+								<h3>Make a Video Call<span class="subtitle">CanvasOnSkype</span></h3>
+							</div>
+						</div>
+
+						<div class="col-md-3 col-sm-6 bottommargin clearfix">
+							<div class="feature-box fbox-center fbox-bg fbox-plain">
+								<div class="fbox-icon">
+									<a href="#"><i class="icon-twitter2"></i></a>
+								</div>
+								<h3>Follow on Twitter<span class="subtitle">2.3M Followers</span></h3>
+							</div>
+						</div>
+
+					</div><!-- Contact Info End -->
+
+				</div>
+
+			</div>
+
+		</section><!-- #content end -->
+
+
+
+<?php
+get_footer();
+?>
