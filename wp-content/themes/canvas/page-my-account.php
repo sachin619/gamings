e22fb481ac610f97f91c255233d41f2b0cc4b8fc
deepak <deepak@infiniteit.biz>
2016-05-10 12:47:59 +0530

    ui

--- wp-content/themes/canvas/page-my-account.php
+++ wp-content/themes/canvas/page-my-account.php
@@ -103,6 +103,7 @@ $userEmail = $userInfo->user_email;
                                 <button ng-click="pagination.prevPage()">Previous</button>
                                 <button ng-click="pagination.nextPage()">Next</button>
                                 <input type="text" class="datepicker startDate sm-form-control tleft" value="<?= $_POST['startDate'] ?>" ng-model="startDate" name="startDate" placeholder="Start Date" />
+                                <span class="input-group-addon">to</span>
                                 <input type="text" class="datepicker endDate sm-form-control tleft" name="endDate" value="<?= $_POST['endDate'] ?>" ng-model="endDate"  placeholder="End Date" />
 
                                 <div class="input-daterange input-group">
