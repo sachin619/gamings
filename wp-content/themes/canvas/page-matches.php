4501239ee3e3212d33dfdee0bc60ceebfb29aa41
deepak <deepak@infiniteit.biz>
2016-05-10 17:47:28 +0530

    ui

--- wp-content/themes/canvas/page-matches.php
+++ wp-content/themes/canvas/page-matches.php
@@ -53,7 +53,7 @@ get_header();
                 <!-- Portfolio Items
                 ============================================= -->
                 <div  ng-init="i=1"  id="portfolio" class="portfolio grid-container portfolio-2 clearfix main-container">
-                    <!-- <article ng-repeat="getPost in getDetails.catPost" class="col-md-4 pf-hide pf-media pf-{{getPost['category'][0]['name']}}">
+                    <article ng-repeat="getPost in getDetails.catPost" class="col-md-4 pf-hide pf-media pf-{{getPost['category'][0]['name']}}">
                         <div ng-init="$parent.j=$parent.i=i+1" class="portfolio-image" >
                             <a href="{{getPost['postLink']}}">
                                 <img src="{{getPost['img']}}" alt="Open Imagination">
@@ -70,7 +70,7 @@ get_header();
                                 {{getPost['start_date']}} - {{getPost['end_date']}}, {{getPost['venue']}}
                             </span>
                         </div>
-                    </article> -->
+                    </article>
 
                     
 
