ebebf8b4c2dc9698f1181ea1fdc33a0d7d031193
deepak <deepak@infiniteit.biz>
2016-05-10 16:16:28 +0530

    ui

--- wp-content/themes/canvas/header.php
+++ wp-content/themes/canvas/header.php
@@ -80,7 +80,7 @@ endif;
                                         <li class="<?= strpos($_SERVER['REQUEST_URI'], 'matches') > 0 ? 'current' : ''; ?>"><a href="<?= get_site_url() ?>/matches"><div><i class=""></i>Matches</div></a>
 
                                         </li>
-                                        <li class="mega-menu <?= strpos($_SERVER['REQUEST_URI'], 'tournaments') > 0 ? 'current' : ''; ?>"><a href="<?= get_site_url() ?>/tournaments"><div><i class=""></i>Tournaments</div></a>
+                                        <li class="<?= strpos($_SERVER['REQUEST_URI'], 'tournaments') > 0 ? 'current' : ''; ?>"><a href="<?= get_site_url() ?>/tournaments"><div><i class=""></i>Tournaments</div></a>
                                         </li>
                                     </ul>
                                 </li>
