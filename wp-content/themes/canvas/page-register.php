243c5052f496ee83213f2418a8fff75737e9a958
deepak <deepak@infiniteit.biz>
2016-05-13 17:23:08 +0530

    ui

--- wp-content/themes/canvas/page-register.php
+++ wp-content/themes/canvas/page-register.php
@@ -12,11 +12,11 @@ get_header();
 <section id="page-title">
 
     <div class="container clearfix">
-        <h1>My Account</h1>
+        <h1>Login / Register</h1>
         <ol class="breadcrumb">
             <li><a href="#">Home</a></li>
             <li><a href="#">Pages</a></li>
-            <li class="active">Login</li>
+            <li class="active">Login / Register</li>
         </ol>
     </div>
 
