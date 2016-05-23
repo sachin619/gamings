d29adfbfbc8798507256dd76f50c2b1bff35f818
VidnyanInfiny <vidnyan@infiniteit.biz>
2016-05-23 15:23:42 +0530

    ui

--- wp-content/themes/canvas/page-matches.php
+++ wp-content/themes/canvas/page-matches.php
@@ -109,7 +109,7 @@ get_header();
                                 <td> {{matches['onlySDate']}}<br> {{matches['matchStartTime']}} - {{matches['matchEndTime']}} <br> <!--<i ng-if="matches['venue'] != ''" class="icon-map-marker2"></i>--> {{matches['venue']}}</a></td>
                                 <td><a href="{{matches['siteUrl'] + '/tournaments/' + matches['tournament_name']['post_name']}}">{{matches['tournament_name']['post_title']}}</a></td>
                                 <td  ng-repeat-start="teams in matches['select_teams']"><b>{{teams['team_name']['post_title']}}</b> </td>
-                                <td  ng-repeat-end> <input type="text" ng-model="$parent.points[teams['team_name']['ID']]" width="100%" placeholder="Add Tarde" ><br>Your Trade : {{getDetails['tradeTotal'][matches['id']][$index][0]['total']}}
+                                <td  ng-repeat-end> <input type="text" ng-model="$parent.points[teams['team_name']['ID']]" style="width: 100%;" placeholder=" Add Tarde" ><br>Your Trade : {{getDetails['tradeTotal'][matches['id']][$index][0]['total']}}
                                     <b style="color:#d43f3a" ng-if="teams['winner'] == 'Yes'">Win</b> 
                                     <b style="color:#d43f3a" ng-if="teams['winner'] == 'No' && matches['points_distributed'] == 'Yes'">Lose</b>  </td>
                                 <td><a href="#" onclick="return false" ng-click="tradeMatch(matches['postLink'], matches['id'], points, getDetails.catPost[0]['uid'])" class="btn btn-danger">Trade</a></td>
