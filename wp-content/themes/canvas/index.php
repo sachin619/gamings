5f1c1f5b04fa7e904d01b5fe8f99d11ef64b5022
VidnyanInfiny <vidnyan@infiniteit.biz>
2016-05-23 15:09:45 +0530

    ui

--- wp-content/themes/canvas/index.php
+++ wp-content/themes/canvas/index.php
@@ -71,9 +71,9 @@ get_header();
                         <tbody> 
 <!--                     {{home['upcomingMatches']['catPost']}}-->
                             <tr ng-repeat="matches in homeMatchListing['upcomingMatches']['catPost']">
-                                <td><i class="icon-time"></i>{{matches['onlySDate']}} {{matches['matchStartTime']}} - {{matches['matchEndTime']}} <br> <i ng-if="matches['venue']!=''" class="icon-map-marker2"></i> {{matches['venue']}}</a></td>          
+                                <td> {{matches['onlySDate']}}<br> {{matches['matchStartTime']}} - {{matches['matchEndTime']}} <br> <!--<i ng-if="matches['venue']!=''" class="icon-map-marker2"></i>--> {{matches['venue']}}</a></td>
                                 <td><a href="{{matches['siteUrl']+'/tournaments/'+matches['tournament_name']['post_name']}}">{{matches['tournament_name']['post_title']}}</a></td>
-                                <td  ng-repeat-start="teams in matches['select_teams']">{{teams['team_name']['post_title']}}: </td>
+                                <td  ng-repeat-start="teams in matches['select_teams']"><b>{{teams['team_name']['post_title']}}</b></td>
                                 <td  ng-repeat-end> <input type="text" ng-model="$parent.points[teams['team_name']['ID']]" ><br>Your Trade : {{homeMatchListing['upcomingMatches']['tradeTotal'][matches['id']][$index][0]['total']}} </td>
                                 <td><a href="#" onclick="return false" ng-click="tradeMatch(matches['postLink'], matches['id'], points,homeMatchListing['upcomingMatches']['catPost'][0]['uid'])" class="btn btn-danger">Trade</a></td>
                                 <td>{{matches["total_bets"]}}</td>
