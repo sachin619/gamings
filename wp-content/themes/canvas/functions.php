cd952d73bfb8effd26bee249835ed30e12a906fd
sachin619 <echo.smb@gmail.com>
2016-05-31 16:06:02 +0530

    hide menu

--- wp-content/themes/canvas/functions.php
+++ wp-content/themes/canvas/functions.php
@@ -679,3 +679,17 @@ function adminDistribution($userid) {
         endif;
     endforeach;
 }
+function remove_menus() {
+    if (get_current_user_id() != 1) {
+        remove_menu_page('edit.php');
+        remove_menu_page('upload.php');                 //Media
+        remove_menu_page('edit.php?post_type=page');    //Pages
+        remove_menu_page('edit-comments.php');          //Comments
+        remove_menu_page('themes.php');                 //Appearance
+        remove_menu_page('plugins.php');                //Plugins
+        remove_menu_page('edit.php?post_type=acf-field-group');
+        remove_menu_page('tools.php');
+    }
+}
+
+add_action('admin_menu', 'remove_menus');
