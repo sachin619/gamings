0c68a58c1ac82e0655ca3e713418f6432e16121e
VidnyanInfiny <vidnyan@infiniteit.biz>
2016-06-16 16:54:00 +0530

    ui

--- wp-content/themes/canvas/index.php
+++ wp-content/themes/canvas/index.php
@@ -168,150 +168,104 @@ get_header();
     <section id="content">
         <div class="content-wrap">
             <div class="container clearfix">
-    <div id="oc-posts" class="owl-carousel posts-carousel carousel-widget" data-margin="20" data-nav="true" data-pagi="false" data-items-xxs="1" data-items-xs="2" data-items-sm="3" data-items-md="4">
+                <div id="oc-posts" class="owl-carousel posts-carousel carousel-widget" data-margin="20" data-nav="true" data-pagi="false" data-items-xxs="1" data-items-xs="2" data-items-sm="3" data-items-md="4">
+                        <div class="oc-item">
+                                <div class="ipost clearfix">
+                                        <div class="entry-image">
+                                                <a href="images/portfolio/full/1.jpg" data-lightbox="image"><img class="image_fade" src="images/blog/grid/1.jpg" alt="Standard Post with Image"></a>
+                                        </div>
+                                        <div class="entry-title">
+                                                <h3><a href="blog-single.html">This is a Standard post with another Preview Image</a></h3>
+                                        </div>
+                                        <ul class="entry-meta clearfix">
+                                                <li><i class="icon-calendar3"></i> 5th May 2014</li>
+                                                <li><a href="blog-single.html#comments"><i class="icon-comments"></i> 6</a></li>
+                                                <li><a href="#"><i class="icon-camera-retro"></i></a></li>
+                                        </ul>
+                                        <div class="entry-content">
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in.</p>
+                                        </div>
+                                </div>
+                        </div>
+                    
+                    <div class="oc-item">
+                                <div class="ipost clearfix">
+                                        <div class="entry-image">
+                                                <a href="images/portfolio/full/1.jpg" data-lightbox="image"><img class="image_fade" src="images/blog/grid/1.jpg" alt="Standard Post with Image"></a>
+                                        </div>
+                                        <div class="entry-title">
+                                                <h3><a href="blog-single.html">This is a Standard post with another Preview Image</a></h3>
+                                        </div>
+                                        <ul class="entry-meta clearfix">
+                                                <li><i class="icon-calendar3"></i> 5th May 2014</li>
+                                                <li><a href="blog-single.html#comments"><i class="icon-comments"></i> 6</a></li>
+                                                <li><a href="#"><i class="icon-camera-retro"></i></a></li>
+                                        </ul>
+                                        <div class="entry-content">
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in.</p>
+                                        </div>
+                                </div>
+                        </div>
+                    
+                    <div class="oc-item">
+                                <div class="ipost clearfix">
+                                        <div class="entry-image">
+                                                <a href="images/portfolio/full/1.jpg" data-lightbox="image"><img class="image_fade" src="images/blog/grid/1.jpg" alt="Standard Post with Image"></a>
+                                        </div>
+                                        <div class="entry-title">
+                                                <h3><a href="blog-single.html">This is a Standard post with another Preview Image</a></h3>
+                                        </div>
+                                        <ul class="entry-meta clearfix">
+                                                <li><i class="icon-calendar3"></i> 5th May 2014</li>
+                                                <li><a href="blog-single.html#comments"><i class="icon-comments"></i> 6</a></li>
+                                                <li><a href="#"><i class="icon-camera-retro"></i></a></li>
+                                        </ul>
+                                        <div class="entry-content">
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in.</p>
+                                        </div>
+                                </div>
+                        </div>
+                    
+                    <div class="oc-item">
+                                <div class="ipost clearfix">
+                                        <div class="entry-image">
+                                                <a href="images/portfolio/full/1.jpg" data-lightbox="image"><img class="image_fade" src="images/blog/grid/1.jpg" alt="Standard Post with Image"></a>
+                                        </div>
+                                        <div class="entry-title">
+                                                <h3><a href="blog-single.html">This is a Standard post with another Preview Image</a></h3>
+                                        </div>
+                                        <ul class="entry-meta clearfix">
+                                                <li><i class="icon-calendar3"></i> 5th May 2014</li>
+                                                <li><a href="blog-single.html#comments"><i class="icon-comments"></i> 6</a></li>
+                                                <li><a href="#"><i class="icon-camera-retro"></i></a></li>
+                                        </ul>
+                                        <div class="entry-content">
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in.</p>
+                                        </div>
+                                </div>
+                        </div>
+                    
+                    <div class="oc-item">
+                                <div class="ipost clearfix">
+                                        <div class="entry-image">
+                                                <a href="images/portfolio/full/1.jpg" data-lightbox="image"><img class="image_fade" src="images/blog/grid/1.jpg" alt="Standard Post with Image"></a>
+                                        </div>
+                                        <div class="entry-title">
+                                                <h3><a href="blog-single.html">This is a Standard post with another Preview Image</a></h3>
+                                        </div>
+                                        <ul class="entry-meta clearfix">
+                                                <li><i class="icon-calendar3"></i> 5th May 2014</li>
+                                                <li><a href="blog-single.html#comments"><i class="icon-comments"></i> 6</a></li>
+                                                <li><a href="#"><i class="icon-camera-retro"></i></a></li>
+                                        </ul>
+                                        <div class="entry-content">
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in.</p>
+                                        </div>
+                                </div>
+                        </div>
 
-						<div class="oc-item">
-							<div class="ipost clearfix">
-								<div class="entry-image">
-									<a href="images/portfolio/full/17.jpg" data-lightbox="image"><img class="image_fade" src="images/blog/grid/17.jpg" alt="Standard Post with Image"></a>
-								</div>
-								<div class="entry-title">
-									<h3><a href="blog-single.html">This is a Standard post with a Preview Image</a></h3>
-								</div>
-								<ul class="entry-meta clearfix">
-									<li><i class="icon-calendar3"></i> 10th Feb 2014</li>
-									<li><a href="blog-single.html#comments"><i class="icon-comments"></i> 13</a></li>
-									<li><a href="#"><i class="icon-camera-retro"></i></a></li>
-								</ul>
-								<div class="entry-content">
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in.</p>
-								</div>
-							</div>
-						</div>
 
-						<div class="oc-item">
-							<div class="ipost clearfix">
-								<div class="entry-image">
-									<iframe src="http://player.vimeo.com/video/87701971" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-								</div>
-								<div class="entry-title">
-									<h3><a href="blog-single-full.html">This is a Standard post with a Vimeo Video</a></h3>
-								</div>
-								<ul class="entry-meta clearfix">
-									<li><i class="icon-calendar3"></i> 16th Feb 2014</li>
-									<li><a href="blog-single-full.html#comments"><i class="icon-comments"></i> 19</a></li>
-									<li><a href="#"><i class="icon-film"></i></a></li>
-								</ul>
-								<div class="entry-content">
-									<p>Asperiores, tenetur, blanditiis, quaerat odit ex exercitationem pariatur quibusdam veritatis quisquam!</p>
-								</div>
-							</div>
-						</div>
-
-						<div class="oc-item">
-							<div class="ipost clearfix">
-								<div class="entry-image">
-									<div class="fslider" data-arrows="false" data-lightbox="gallery">
-										<div class="flexslider">
-											<div class="slider-wrap">
-												<div class="slide"><a href="images/portfolio/full/10.jpg" data-lightbox="gallery-item"><img class="image_fade" src="images/blog/grid/10.jpg" alt="Standard Post with Gallery"></a></div>
-												<div class="slide"><a href="images/portfolio/full/20.jpg" data-lightbox="gallery-item"><img class="image_fade" src="images/blog/grid/20.jpg" alt="Standard Post with Gallery"></a></div>
-												<div class="slide"><a href="images/portfolio/full/21.jpg" data-lightbox="gallery-item"><img class="image_fade" src="images/blog/grid/21.jpg" alt="Standard Post with Gallery"></a></div>
-											</div>
-										</div>
-									</div>
-								</div>
-								<div class="entry-title">
-									<h3><a href="blog-single-small.html">This is a Standard post with a Slider Gallery</a></h3>
-								</div>
-								<ul class="entry-meta clearfix">
-									<li><i class="icon-calendar3"></i> 24th Feb 2014</li>
-									<li><a href="blog-single-small.html#comments"><i class="icon-comments"></i> 21</a></li>
-									<li><a href="#"><i class="icon-picture"></i></a></li>
-								</ul>
-								<div class="entry-content">
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione, voluptatem, dolorem animi nisi autem!</p>
-								</div>
-							</div>
-						</div>
-
-						<div class="oc-item">
-							<div class="ipost clearfix">
-								<div class="entry-image clearfix">
-									<iframe width="100%" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/115823769&amp;auto_play=false&amp;hide_related=true&amp;visual=true"></iframe>
-								</div>
-								<div class="entry-title">
-									<h3><a href="blog-single.html">This is an Embedded Audio Post</a></h3>
-								</div>
-								<ul class="entry-meta clearfix">
-									<li><i class="icon-calendar3"></i> 28th Apr 2014</li>
-									<li><a href="blog-single.html#comments"><i class="icon-comments"></i> 16</a></li>
-									<li><a href="#"><i class="icon-music2"></i></a></li>
-								</ul>
-								<div class="entry-content">
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione, voluptatem, dolorem animi nisi!</p>
-								</div>
-							</div>
-						</div>
-
-						<div class="oc-item">
-							<div class="ipost clearfix">
-								<div class="entry-image">
-									<iframe width="560" height="315" src="http://www.youtube.com/embed/SZEflIVnhH8" frameborder="0" allowfullscreen></iframe>
-								</div>
-								<div class="entry-title">
-									<h3><a href="blog-single-full.html">This is a Standard post with a Youtube Video</a></h3>
-								</div>
-								<ul class="entry-meta clearfix">
-									<li><i class="icon-calendar3"></i> 30th Apr 2014</li>
-									<li><a href="blog-single-full.html#comments"><i class="icon-comments"></i> 34</a></li>
-									<li><a href="#"><i class="icon-film"></i></a></li>
-								</ul>
-								<div class="entry-content">
-									<p>Asperiores, tenetur, blanditiis, quaerat odit ex exercitationem pariatur quibusdam veritatis quisquam!</p>
-								</div>
-							</div>
-						</div>
-
-						<div class="oc-item">
-							<div class="ipost clearfix">
-								<div class="entry-image">
-									<a href="images/portfolio/full/1.jpg" data-lightbox="image"><img class="image_fade" src="images/blog/grid/1.jpg" alt="Standard Post with Image"></a>
-								</div>
-								<div class="entry-title">
-									<h3><a href="blog-single.html">This is a Standard post with another Preview Image</a></h3>
-								</div>
-								<ul class="entry-meta clearfix">
-									<li><i class="icon-calendar3"></i> 5th May 2014</li>
-									<li><a href="blog-single.html#comments"><i class="icon-comments"></i> 6</a></li>
-									<li><a href="#"><i class="icon-camera-retro"></i></a></li>
-								</ul>
-								<div class="entry-content">
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in.</p>
-								</div>
-							</div>
-						</div>
-
-						<div class="oc-item">
-							<div class="ipost clearfix">
-								<div class="entry-image clearfix">
-									<iframe width="100%" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/17521446&amp;auto_play=false&amp;hide_related=false&amp;visual=true"></iframe>
-								</div>
-								<div class="entry-title">
-									<h3><a href="blog-single.html">This is another Embedded Audio Post</a></h3>
-								</div>
-								<ul class="entry-meta clearfix">
-									<li><i class="icon-calendar3"></i> 15th May 2014</li>
-									<li><a href="blog-single.html#comments"><i class="icon-comments"></i> 2</a></li>
-									<li><a href="#"><i class="icon-music2"></i></a></li>
-								</ul>
-								<div class="entry-content">
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione, voluptatem, dolorem animi nisi autem!</p>
-								</div>
-							</div>
-						</div>
-
-					</div>
+                </div>
             </div>
         </div>
     </section>
